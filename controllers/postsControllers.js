027f1abbe88b1fda0f1fccf81d79006837fa613c
MorrisKehbel <morris.kehbel@gmail.com>
2025-06-26 19:44:17 +0200

    adjusted postscontrollers

--- controllers/postsControllers.js
+++ controllers/postsControllers.js
@@ -1,18 +1,5 @@
 import { client } from "../db/dbConnections.js";
 
-const posts = [
-  {
-    id: 1,
-    title: "First Post",
-    content: "This is the content of the first post.",
-  },
-  {
-    id: 2,
-    title: "Second Post",
-    content: "This is the content of the second post.",
-  },
-];
-
 export const getAllPosts = async (req, res) => {
   try {
     const results = await client.query("SELECT * FROM posts");
@@ -31,17 +18,14 @@ export const getAllPosts = async (req, res) => {
 export const getPostById = async (req, res) => {
   try {
     const { id } = req.params;
-
-    if (!id) {
-      return res.status(400).json({ error: "Missing posts ID" });
-    }
+    const postID = parseInt(id);
 
     const results = await client.query("SELECT * FROM posts WHERE id = $1", [
-      id,
+      postID,
     ]);
 
     if (results.rows.length === 0) {
-      return res.status(404).json({ message: "posts not found" });
+      return res.status(404).json({ message: "Posts not found" });
     }
 
     return res.status(200).json(results.rows[0]);
@@ -51,39 +35,78 @@ export const getPostById = async (req, res) => {
   }
 };
 
-export const createPost = (req, res) => {
-  const { title, content } = req.body;
-  if (!title || !content) {
-    return res.status(400).json({ message: "Title and content are required" });
+export const createPost = async (req, res) => {
+  try {
+    const { author, title, content, cover } = req.body;
+    if (!author || !title || !content || !cover) {
+      return res.status(400).json({
+        message: "The author, title, content and cover are required.",
+      });
+    }
+    const results = await client.query(
+      "INSERT INTO posts (author, title, content, cover) VALUES ($1, $2, $3, $4) RETURNING *",
+      [author, title, content, cover]
+    );
+    return res.status(201).json(results.rows[0]);
+  } catch (error) {
+    console.error("Error creating post:", error);
+    return res.status(500).json({ message: "Internal server error" });
   }
-  const newPost = {
-    id: posts.length + 1,
-    title,
-    content,
-  };
-  posts.push(newPost);
-  res.status(201).json(newPost);
 };
 
-export const updatePost = (req, res) => {
-  const { id } = req.params;
-  const { title, content } = req.body;
-  const postIndex = posts.findIndex((p) => p.id === parseInt(id));
-  if (postIndex === -1) {
-    return res.status(404).json({ message: "Post not found" });
+export const updatePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { author, title, content, cover } = req.body;
+
+    if (!author || !title || !content || !cover) {
+      return res
+        .status(400)
+        .json({ message: "New author, title, content and cover are required" });
+    }
+
+    const postID = parseInt(id);
+    const postExists = await client.query("SELECT * FROM posts WHERE id = $1", [
+      postID,
+    ]);
+    if (postExists.rows.length === 0) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    const results = await client.query(
+      "UPDATE posts SET author = $1, title = $2, content = $3, cover = $4 WHERE id = $5 RETURNING *",
+      [author, title, content, cover, postID]
+    );
+
+    res.status(200).json(results.rows[0]);
+  } catch (error) {
+    console.error("Error updating user:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
-  if (!title || !content) {
-    return res.status(400).json({ message: "Title and content are required" });
-  }
-  posts[postIndex] = { id: parseInt(id), title, content };
-  res.json(posts[postIndex]);
 };
 
-export const deletePost = (req, res) => {
-  const { id } = req.params;
-  const postIndex = posts.findIndex((p) => p.id === parseInt(id));
-  if (postIndex === -1) {
-    return res.status(404).json({ message: "Post not found" });
+export const deletePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const postID = parseInt(id);
+    const postExists = await client.query("SELECT * FROM posts WHERE id = $1", [
+      postID,
+    ]);
+    if (postExists.rows.length === 0) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    const results = await client.query("DELETE FROM posts WHERE id = $1", [
+      postID,
+    ]);
+
+    if (results.rowCount === 1) {
+      return res.status(200).json({ message: "Post deleted successfully!" });
+    }
+
+    res.status(500).json({ message: "Failed to delete post" });
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
-  res.send("Post deleted successfully");
 };
